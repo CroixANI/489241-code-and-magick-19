472f380246ee9892314dd7e8a14d9fe2e20d254a
Alexander Nichiporovich <a.nichiporovich@gmail.com>
2020-01-27 21:45:38 +0300

    Module 2 - Task 1 - Fix code review comments.
    - Rename constants.
    
    - Extract bar drawing into separate function.

--- js/stat.js
+++ js/stat.js
@@ -1,100 +1,122 @@
 'use strict';
 
-var statisticConstants = {
-  window: {
-    startPositionX: 100,
-    startPositionY: 10,
-    width: 420,
-    height: 270,
-    shadowStartPositionDiff: 10,
-    getShadowStartPosition: function () {
-      return {
-        x: statisticConstants.window.startPositionX + statisticConstants.window.shadowStartPositionDiff,
-        y: statisticConstants.window.startPositionY + statisticConstants.window.shadowStartPositionDiff
-      };
-    },
-    getStartPosition: function () {
-      return {
-        x: statisticConstants.window.startPositionX,
-        y: statisticConstants.window.startPositionY
-      };
-    }
-  },
-  text: {
-    fontFamily: '16px PT Mono',
-    fontColor: '#000',
-    lineHeight: 20,
-    x: 15,
-    y: 30,
-    getStartPosition: function () {
-      return {
-        x: statisticConstants.window.getStartPosition().x + statisticConstants.text.x,
-        y: statisticConstants.window.getStartPosition().y + statisticConstants.text.y,
-      };
-    }
-  },
-  bars: {
-    height: 150,
-    columnWidth: 40,
-    columnGap: 50,
-    playerColor: 'rgba(255, 0, 0, 1)',
-    otherColor: 'rgb(0, 0, ${X}, 1)'
-  }
-};
+// Window Position Constants
+var WINDOW_POSITION_X = 100;
+var WINDOW_POSITION_Y = 10;
+var WINDOW_SHADOW_POSITION_INDENT = 10;
+var WINDOW_HEIGHT = 270;
+var WINDOW_WIDTH = 420;
 
-function renderRectangle(ctx, x, y, color, width, height) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, width, height);
+// Window Text Constants
+var TEXT_FONT_FAMILY = '16px PT Mono';
+var TEXT_FONT_COLOR = '#000';
+var TEXT_LINE_HEIGHT = 20;
+var WINDOW_TITLE_POSITION_X = 15;
+var WINDOW_TITLE_POSITION_Y = 30;
+
+// Statistic Bars Constants
+var STATISTIC_ZONE_HEIGHT = 150;
+var FIRST_BAR_LEFT_BUTTOM_INDENT = 40;
+var BAR_WIDTH = 40;
+var BAR_GAP_WIDTH = 50;
+var BAR_PLAYER_COLOR = 'rgba(255, 0, 0, 1)';
+var BAR_OTHER_COLOR = 'hsl(240, ${X}%, 50%)';// 'rgb(0, 0, ${X}, 1)';
+var BAR_POSITION_X = WINDOW_POSITION_X + FIRST_BAR_LEFT_BUTTOM_INDENT;
+var BAR_POSITION_Y = WINDOW_POSITION_Y + WINDOW_HEIGHT - FIRST_BAR_LEFT_BUTTOM_INDENT;
+var BAR_CAPTION_POSITION_Y = Math.floor(BAR_POSITION_Y + FIRST_BAR_LEFT_BUTTOM_INDENT / 2);
+
+function getWindowRectangle() {
+  return {
+    x: WINDOW_POSITION_X,
+    y: WINDOW_POSITION_Y,
+    height: WINDOW_HEIGHT,
+    width: WINDOW_WIDTH,
+    color: 'white'
+  };
 }
 
-function renderMessage(ctx, message, x, y) {
+function getWindowShadowRectangle() {
+  var windowPosition = getWindowRectangle();
+  windowPosition.x += WINDOW_SHADOW_POSITION_INDENT;
+  windowPosition.y += WINDOW_SHADOW_POSITION_INDENT;
+  windowPosition.color = 'rgba(0, 0, 0, 0.7)';
+  return windowPosition;
+}
 
-  ctx.fillStyle = statisticConstants.text.fontColor;
-  ctx.font = statisticConstants.text.fontColor.fontFamily;
+function getWindowTitlePosition() {
+  var windowPosition = getWindowRectangle();
+  return {
+    x: windowPosition.x + WINDOW_TITLE_POSITION_X,
+    y: windowPosition.y + WINDOW_TITLE_POSITION_Y,
+  };
+}
+
+function renderRectangle(ctx, rectangle) {
+  ctx.fillStyle = rectangle.color;
+  ctx.fillRect(rectangle.x, rectangle.y, rectangle.width, rectangle.height);
+}
+
+function renderMessage(ctx, message, position) {
+
+  ctx.fillStyle = TEXT_FONT_COLOR;
+  ctx.font = TEXT_FONT_FAMILY;
   message.split('\n').forEach(function (line, i) {
-    ctx.fillText(line, x, y + statisticConstants.text.lineHeight * i);
+    ctx.fillText(line, position.x, position.y + TEXT_LINE_HEIGHT * i);
   });
 }
 
 function calculateBarHeight(max, current) {
-  return Math.floor((statisticConstants.bars.height * current) / max);
+  return Math.floor((STATISTIC_ZONE_HEIGHT * current) / max);
+}
+
+function getMaxTime(times) {
+  return Math.floor(times.reduce(function (a, b) {
+    return Math.max(a, b);
+  }));
+}
+
+function random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function renderBars(ctx, names, times) {
+  var maxTime = getMaxTime(times);
+
+  for (var i = 0; i < names.length; i++) {
+    var name = names[i];
+    var startXDiff = i * (BAR_WIDTH + BAR_GAP_WIDTH);
+    var currentBarHeight = calculateBarHeight(maxTime, times[i]);
+
+    var color = BAR_OTHER_COLOR;
+    if (name === 'Вы') {
+      color = BAR_PLAYER_COLOR;
+    } else {
+      // example took here https://css-tricks.com/hsl-hsla-is-great-for-programmatic-color-control/
+      color = color.replace('${X}', random(0, 100));
+    }
+
+    var currentBarRectangle = {
+      x: BAR_POSITION_X + startXDiff,
+      y: BAR_POSITION_Y - currentBarHeight,
+      height: currentBarHeight,
+      width: BAR_WIDTH,
+      color: color
+    };
+
+    // visual start of bar = window start position + total window height - firstBardBordersDiff, where firstBardBordersDiff approx diff between bar visual begin and window buttom
+    renderRectangle(ctx, currentBarRectangle);
+    renderMessage(ctx, name, { x: currentBarRectangle.x, y: BAR_CAPTION_POSITION_Y });
+  }
 }
 
 window.renderStatistics = function (ctx, names, times) {
-  var shadowStartPosition = statisticConstants.window.getShadowStartPosition();
-  var startPosition = statisticConstants.window.getStartPosition();
-  var mainTextStartPosition = statisticConstants.text.getStartPosition();
-
-
-  var firstBardBordersDiff = 40;
-  var barStartY = startPosition.y + statisticConstants.window.height - firstBardBordersDiff;
-  var maxTime = Math.floor(times.reduce(function (a, b) {
-    return Math.max(a, b);
-  }));
-
   // draw window dialog
-  renderRectangle(ctx, shadowStartPosition.x, shadowStartPosition.y, 'rgba(0, 0, 0, 0.7)', statisticConstants.window.width, statisticConstants.window.height);
-  renderRectangle(ctx, startPosition.x, startPosition.y, 'white', statisticConstants.window.width, this.statisticConstants.window.height);
+  renderRectangle(ctx, getWindowShadowRectangle());
+  renderRectangle(ctx, getWindowRectangle());
+
   // display main text
-  renderMessage(ctx, 'Ура вы победили! \nСписок результатов: ', mainTextStartPosition.x, mainTextStartPosition.y);
+  renderMessage(ctx, 'Ура вы победили! \nСписок результатов: ', getWindowTitlePosition());
 
   // draw histogram e.g. bars
-  for (var i = 0; i < names.length; i++) {
-    var name = names[i];
-    var barHeight = calculateBarHeight(maxTime, times[i]);
-    var startXDiff = i * (statisticConstants.bars.columnWidth + statisticConstants.bars.columnGap);
-    var barStartX = startPosition.x + firstBardBordersDiff + startXDiff;
-
-    var color = statisticConstants.bars.otherColor;
-    if (name === 'Вы') {
-      color = statisticConstants.bars.playerColor;
-    } else {
-      // example took here https://stackoverflow.com/questions/20790579/wont-math-floormath-random-255-generate-uneven-probabilities
-      color = color.replace('${X}', Math.floor(Math.random() * 256));
-    }
-
-    // visual start of bar = window start position + total window height - firstBardBordersDiff, where firstBardBordersDiff approx diff between bar visual begin and window buttom
-    renderRectangle(ctx, barStartX, barStartY - barHeight, color, statisticConstants.bars.columnWidth, barHeight);
-    renderMessage(ctx, name, barStartX, barStartY + firstBardBordersDiff / 2);
-  }
+  renderBars(ctx, names, times);
 };
