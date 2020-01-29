67699c37863ee5acce32dfb4012eaea9a3a49a38
Alexander Nichiporovich <a.nichiporovich@gmail.com>
2020-01-29 21:00:56 +0300

    Module 3 - Task 1 - Fix code review comments - move all selectors to constants.

--- js/setup.js
+++ js/setup.js
@@ -1,10 +1,20 @@
 'use strict';
 
+// Wizards constants
 var NAMES = ['Иван', 'Хуан Себастьян', 'Мария', 'Кристоф', 'Виктор', 'Юлия', 'Люпита', 'Вашингтон'];
 var SURNAMES = ['да Марья', 'Верон', 'Мирабелла', 'Вальц', 'Онопко', 'Топольницкая', 'Нионго', 'Ирвинг'];
 var COAT_COLORS = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
 var EYES_COLORS = ['black', 'red', 'blue', 'yellow', 'green'];
 var WIZARDS_COUNT = 4;
+
+// Html Elements Selectors
+var SETUP_DIALOG_SELECTOR = '.setup';
+var WIZARD_SIMILAR_REGION_SELECTOR = '.setup-similar';
+var WIZARD_SIMILAR_ELEMENT_SELECTOR = '.setup-similar-list';
+var WIZARD_TEMPLATE_ELEMENT_SELECTOR = '#similar-wizard-template';
+var WIZARD_NAME_ELEMENT_SELECTOR = '.setup-similar-label';
+var WIZARD_COAT_ELEMENT_SELECTOR = '.wizard-coat';
+var WIZARD_EYES_ELEMENT_SELECTOR = '.wizard-eyes';
 
 function random(min, max) {
   return Math.floor(Math.random() * (max - min + 1) + min);
@@ -36,16 +46,16 @@ function generateWizards() {
 
 function renderWizard(template, wizard) {
   var wizardElement = template.cloneNode(true);
-  wizardElement.querySelector('.setup-similar-label').textContent = wizard.name;
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+  wizardElement.querySelector(WIZARD_NAME_ELEMENT_SELECTOR).textContent = wizard.name;
+  wizardElement.querySelector(WIZARD_COAT_ELEMENT_SELECTOR).style.fill = wizard.coatColor;
+  wizardElement.querySelector(WIZARD_EYES_ELEMENT_SELECTOR).style.fill = wizard.eyesColor;
   return wizardElement;
 }
 
 function renderWizards() {
-  var similarListElement = document.querySelector('.setup-similar-list');
+  var similarListElement = document.querySelector(WIZARD_SIMILAR_ELEMENT_SELECTOR);
   var wizardTemplate = document
-    .querySelector('#similar-wizard-template')
+    .querySelector(WIZARD_TEMPLATE_ELEMENT_SELECTOR)
     .content;
 
   var wizards = generateWizards();
@@ -58,8 +68,8 @@ function renderWizards() {
 
 function showSetupDialog() {
   renderWizards();
-  document.querySelector('.setup').classList.remove('hidden');
-  document.querySelector('.setup-similar').classList.remove('hidden');
+  document.querySelector(SETUP_DIALOG_SELECTOR).classList.remove('hidden');
+  document.querySelector(WIZARD_SIMILAR_REGION_SELECTOR).classList.remove('hidden');
 }
 
 showSetupDialog();
